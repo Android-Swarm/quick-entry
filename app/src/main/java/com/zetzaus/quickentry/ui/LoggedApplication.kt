b731ea0cf9dc9393e477dceb37e3f47501604226
Sebastian <sebastianliando@gmail.com>
2020-08-10 17:54:28 +0800

    Hide bugfender API key

--- app/src/main/java/com/zetzaus/quickentry/ui/LoggedApplication.kt
+++ app/src/main/java/com/zetzaus/quickentry/ui/LoggedApplication.kt
@@ -3,13 +3,14 @@ package com.zetzaus.quickentry.ui
 import android.app.Application
 import com.bugfender.sdk.Bugfender
 import com.zetzaus.quickentry.BuildConfig
+import com.zetzaus.quickentry.R
 
 class LoggedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.BUILD_TYPE == "release") {
-            Bugfender.init(this, "AjGfCRGc5iQh5TmlyL5ZRND6iG2qJ5O3", BuildConfig.DEBUG)
+            Bugfender.init(this, getString(R.string.bugfender_api_key), BuildConfig.DEBUG)
             Bugfender.enableCrashReporting()
             Bugfender.enableUIEventLogging(this)
             Bugfender.enableLogcatLogging()
