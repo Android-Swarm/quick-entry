9054c5676d469aeb7de95d6e468c829449943cb2
Sebastian <sebastianliando@gmail.com>
2020-08-11 23:05:22 +0800

    Add animated demos

--- app/src/main/java/com/zetzaus/quickentry/ui/EntryViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.zetzaus.quickentry.ui
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import com.zetzaus.quickentry.database.EntrySpotRepository
-
-open class EntryViewModel(application: Application) : AndroidViewModel(application) {
-    protected val repository = EntrySpotRepository(application.baseContext)
-}
